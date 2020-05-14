4f29eb93108c465f5cf1d601dd83ac73be77c37b
Aditya Abhay Gaikwad <63276445+aditya-github02@users.noreply.github.com>
2020-05-14 16:33:05 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,11 +1,11 @@
-(function() {
+(function(window) {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0; i < names.length; i++) {
-        var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]);
+    for (var i in names) {
+        var firstLetter =((names[i]).charAt(0)).toLowerCase();
+        if (firstLetter == 'j') {
+            window.byeSpeaker.speak(names[i]);
         } else {
-            helloSpeaker.speak(names[i]);
+            window.helloSpeaker.speak(names[i]);
         }
     }
-})();
+})(window);
