451e3018029b227ecc176d549dccc232812b5e66
Aditya Abhay Gaikwad <63276445+aditya-github02@users.noreply.github.com>
2020-05-14 16:51:02 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 (function(window) {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i in names) {
+    for (var i = 0; i< names.length; i++) {
         var firstLetter =((names[i]).charAt(0)).toLowerCase();
         if (firstLetter == 'j') {
             window.byeSpeaker.speak(names[i]);
